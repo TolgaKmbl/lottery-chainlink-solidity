bc5c583b7d3a675e43247c8a095294022a3847bc
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2021-08-09 22:07:37 +0300

    Update Lottery_test.js

--- test/Lottery_test.js
+++ test/Lottery_test.js
@@ -15,7 +15,7 @@ contract('Lottery', accounts => {
     let lottery, vrfCoordinatorMock, link, keyhash, fee, mockPriceFeed
 
     describe('#requests a random number', () => {
-        let price = '200000000000'
+        let price = '200000000000' // Seting the initial price of Eth/Usd = $2000
         beforeEach(async () => {
             keyHash = '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4'
             fee = '100000000000000000'
@@ -28,8 +28,8 @@ contract('Lottery', accounts => {
             assert(await lottery.lotteryState() == 1)
         })
         it('Correctly gets the entrance fee', async () => {
-            let entranceFee = await lottery.getEntranceFee()
-            assert.equal(entranceFee.toString(), '25000000000000000')
+            let entranceFee = await lottery.getEntranceFee() // Our entrance fee is $50 hardcoded
+            assert.equal(entranceFee.toString(), '25000000000000000')  // The fee must be 0.25 eth 
         })
         it('Disallows entrants without enough money', async () => {
             await lottery.startLottery({from: defaultAccount})
@@ -43,9 +43,10 @@ contract('Lottery', accounts => {
             lottery.enter({ from: player1, value: entranceFee.toString() })
             lottery.enter({ from: player2, value: entranceFee.toString() })
             lottery.enter({ from: player3, value: entranceFee.toString() })
+            // Lottery smart contract requires link token to pay fees (to use the functions from Mock Oracle Contracts)
             await link.transfer(lottery.address, web3.utils.toWei('1', 'ether'), { from: defaultAccount })
             let transaction = await lottery.endLottery({ from: defaultAccount })
-            let requestId = transaction.receipt.rawLogs[3].topics[0]
+            let requestId = transaction.receipt.rawLogs[3].topics[0] // the reference of the requestId in the tx
 
             // callBackWithRandomness(bytes32 requestId, uint256 randomness, address consumerContract)
             // randomness being 3 means that 'uint256 index = randomness % players.length = 0' => Players[0] = player1
